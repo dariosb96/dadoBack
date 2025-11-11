10850ac538aad61be3c93f80c784d6e3e372e9ff
Dario Sosa <xdsb.tstx@gmail.com>
2025-11-11 12:48:32 -0600

    v1.1.0.

--- db.js
+++ db.js
@@ -1,24 +1,40 @@
+// require("dotenv").config();
+// const { Sequelize } = require("sequelize");
+
+// const isProduction = process.env.NODE_ENV === "production";
+
+// const sequelize = new Sequelize(
+//   isProduction
+//     ? process.env.DATABASE_URL
+//     : `postgres://${process.env.DB_USER_LOCAL}:${process.env.DB_PASSWORD_LOCAL}@${process.env.DB_HOST_LOCAL}:${process.env.DB_PORT_LOCAL}/${process.env.DB_NAME_LOCAL}`,
+//   {
+//     dialect: "postgres",
+//     logging: false,
+//     dialectOptions: isProduction
+//       ? {
+//           ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//           },
+//         }
+//       : {},
+//   }
+// );
+
+// module.exports = sequelize;
+
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const sequelize = new Sequelize(
-  isProduction
-    ? process.env.DATABASE_URL
-    : `postgres://${process.env.DB_USER_LOCAL}:${process.env.DB_PASSWORD_LOCAL}@${process.env.DB_HOST_LOCAL}:${process.env.DB_PORT_LOCAL}/${process.env.DB_NAME_LOCAL}`,
-  {
-    dialect: "postgres",
-    logging: false,
-    dialectOptions: isProduction
-      ? {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        }
-      : {},
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;
