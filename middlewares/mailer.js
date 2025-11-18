aeb5b85493db57eea4fd35e9398cf0ae978d73c2
Dario Sosa <xdsb.tstx@gmail.com>
2025-11-17 18:45:26 -0600

    resend v1.1

--- middlewares/mailer.js
+++ middlewares/mailer.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendEmail(to, subject, html) {
   try {
     const response = await resend.emails.send({
-      from: "Daddo <onboarding@resend.dev>",
+      from: "Daddo <daddo@resend.dev>",
       to,
       subject,
       html,
